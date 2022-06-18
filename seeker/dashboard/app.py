9fa5aaf937e202eb96f5c0d3b32cab3cfbf16c64
papsebestyen <pap.sebestyen@gmail.com>
2022-06-18 13:09:28 +0200

    final project management

--- seeker/dashboard/app.py
+++ seeker/dashboard/app.py
@@ -1,96 +1,125 @@
 import streamlit as st
 from seeker.project_config import ProjectConfig, load_all_config
 
-
-def set_session_state(state_name: str, value):
-    st.session_state[state_name] = value
-
-
 st.set_page_config(
     page_title="Seeker",
     page_icon="👋",
 )
 
-st.title("Seeker")
+DEBUG = True
+DEFAULT_PROJECT = "Select project"
 
-st.write("Select a project or upload a new one.")
+if "app_state" not in st.session_state:
+    st.session_state["app_state"] = "select_project"
+if "project" not in st.session_state:
+    st.session_state["project"] = DEFAULT_PROJECT
+if "project_name" not in st.session_state:
+    st.session_state["project_name"] = None
+if "project_type" not in st.session_state:
+    st.session_state["project_type"] = None
+if "query_search" not in st.session_state:
+    st.session_state["query_search"] = None
 
-# UPLOAD PROJECT
-selector_project = st.selectbox(
-    label="Select project",
-    options=["Select a project", *[c.name for c in load_all_config()]],
-    # format_func=lambda opt: opt.name if isinstance(opt, ProjectConfig) else opt,
-)
 
-if selector_project != "Select project":
-    project_conf = ProjectConfig(name=selector_project)
+def sidebar_new_project():
+    st.text_input(label="Project name", key="project_name", value="")
+    st.selectbox(label="Project type", key="project_type", options=["text", "image"])
+    uploaded_files = st.file_uploader(
+        label="Files",
+        key="file_uploader",
+        # type=TYPE_CONF[project_type],
+        accept_multiple_files=True,
+    )
+
+    def callback():
+        project_name = st.session_state["project_name"]
+        project_type = st.session_state["project_type"]
+        if project_name != "" and project_name is not None:
+            conf = ProjectConfig(name=project_name, dtype=project_type)
+            conf.save_config()
+        if uploaded_files is not None:
+            for file in uploaded_files:
+                (conf.data_dir / file.name).write_bytes(file.getvalue())
+        st.session_state["app_state"] = "select_project"
+        st.session_state["project"] = project_name
+
+    st.button("Submit", on_click=callback)
+
+    def callback():
+        st.session_state["app_state"] = "select_project"
+        st.session_state["project"] = DEFAULT_PROJECT
+
+    st.button("Cancel", on_click=callback)
+
+
+def sidebar_show_project():
+    conf = ProjectConfig.from_name(name=st.session_state["project"])
+    st.write(f"Name: {conf.name}")
+    st.write(f"Type: {conf.dtype}")
+    st.write(f"Files: {conf.get_file_count()}")  # TODO
+    st.write(f"Size: X MB")  # TODO
+
+    def callback():
+        ...  # TODO
+
+    st.button(label="Modify", on_click=callback)
+
+
+def sidebar_select_project():
+    st.selectbox(
+        label="Select project",
+        key="project",
+        options=[DEFAULT_PROJECT, *[c.name for c in load_all_config()]],
+    )
+
+    if (
+        st.session_state["project"] != DEFAULT_PROJECT
+        and st.session_state["project"] is not None
+    ):
+        sidebar_show_project()
+
+    def callback():
+        st.session_state["app_state"] = "new_project"
+
+    st.button(label="New project", on_click=callback)
+
+
+def content_text():
+    st.text_input(label="Text search", key="query_search")
+
+
+def content_image():
+    ...
+
+
+def main():
+    sidebar_pages = {
+        "new_project": sidebar_new_project,
+        "select_project": sidebar_select_project,
+    }
+    content_pages = {"text": content_text, "image": content_image}
+
     with st.sidebar:
-        st.write(f"Name: {project_conf.name}")
-        st.write(f"Type: text")  # TODO
-        st.write(f"Files: XY")  # TODO
-        st.write(f"Size: X MB")  # TODO
-else:
-    project_conf = None
+        sidebar_pages[st.session_state["app_state"]]()
 
-_, col2, _, col4, _ = st.columns(5)
+    content_pages["text"]()
 
-btn_modify = col2.button(
-    label="Modify",
-    disabled=True if not isinstance(project_conf, ProjectConfig) else False,
-)  # TODO
+    if DEBUG:
+        st.write(st.session_state["app_state"])
+        st.write(st.session_state["project"])
+        st.write(st.session_state["project_name"])
+        st.write(st.session_state["project_type"])
 
-if "new_project" not in st.session_state:
-    set_session_state("new_project", False)
 
-btn_new = col4.button(label="New project")
-if btn_new:
-    set_session_state("new_project", True)
+# if project_conf is not None:
+#     from seeker.search.numerical.preprocess import preprocess_numeric_data, read_numeric_data, search_numeric
+#     search_input = st.slider(label='Search for num:')
+#     data = read_numeric_data(project_conf)
+#     data.shape
+#     tree = preprocess_numeric_data(data)
+#     tree
+#     dist, ind = search_numeric(tree, [search_input, search_input, search_input])
+#     ind
 
-ph = st.empty()
-if st.session_state["new_project"]:
-    # key='new_project_form'
-    with ph.form(key="new_project_form"):
-        project_name = st.text_input(label="Project name", value="")
-
-        uploaded_files = st.file_uploader(
-            "Upload files",
-            type=["txt"],
-            accept_multiple_files=True,
-        )
-
-        _, col2, _, col4, _ = st.columns(5)
-        btn_submit = col2.form_submit_button(
-            "Submit",
-        )
-        btn_cancel_submit = col4.form_submit_button(
-            "Cancel",
-        )
-
-        if btn_submit:
-            if project_name != '' and project_name is not None:
-                conf = ProjectConfig(name=project_name)
-                conf.save_config()
-            if uploaded_files is not None:
-                for file in uploaded_files:
-                    (conf.data_dir / file.name).write_bytes(file.getvalue())
-            set_session_state('new_project', False)
-            ph.empty()
-        elif btn_cancel_submit:
-            set_session_state('new_project', False)
-            ph.empty()
-else:
-    ph.empty()
-
-if project_conf is not None:
-    from seeker.search.numerical.preprocess import preprocess_numeric_data, read_numeric_data, search_numeric
-    search_input = st.slider(label='Search for num:')
-    data = read_numeric_data(project_conf)
-    data.shape
-    tree = preprocess_numeric_data(data)
-    tree
-    dist, ind = search_numeric(tree, [search_input, search_input, search_input])
-    ind
-    # with st.container():
-    #     st.header('text1')
-    #     st.write('1')
-
+if __name__ == "__main__":
+    main()
--- seeker/project_config.py
+++ seeker/project_config.py
@@ -6,22 +6,47 @@ from hashlib import md5
 if TYPE_CHECKING:
     from pathlib import Path
 
+TYPE_CONF = {
+    "text": ["txt"],
+    "image": ["png", "jpg"],
+}
+
 
 class ProjectConfig:
-    def __init__(self, name=str):
+    def __init__(self, name: str, dtype: str):
         self.name = name.strip()
         self.id = md5(name.encode()).hexdigest()
+        self.dtype = dtype
         self.data_dir = PROJECT_DIR / f"{self.id}"
         self.data_dir.mkdir(exist_ok=True)
 
+    def get_file_count(self) -> int:
+        return len(
+            [f for f in self.data_dir.glob("*.txt")]
+        )  # TODO make universal for all extensions
+
+    def get_dtype(self):
+        suffixes = set([f.suffix.strip(".") for f in self.data_dir.iterdir()])
+        dtype = suffixes.difference({"yaml", "pickle"})
+        assert len(dtype) == 1  # TODO using jpg and png together
+        self.dtype = dtype
+
     def save_config(self, fp: "Path" = None):
         fp = fp or self.data_dir
-        (fp / "config.yaml").write_text(yaml.safe_dump({"name": self.name}))
+        (fp / "config.yaml").write_text(
+            yaml.safe_dump({"name": self.name, "dtype": self.dtype})
+        )
 
     @classmethod
-    def load_config(cls, fp: "Path"):
-        conf = yaml.safe_load(fp.read_text())
-        return cls(name=conf["name"].strip())
+    def from_file(cls, file: "Path"):
+        conf = yaml.safe_load(file.read_text())
+        return cls(name=conf["name"].strip(), dtype=conf["dtype"])
+
+    @classmethod
+    def from_name(cls, name: str):
+        data_dir = PROJECT_DIR / f"{md5(name.encode()).hexdigest()}"
+        return cls.from_file(file=data_dir / "config.yaml")
+
 
 def load_all_config():
-    return [ProjectConfig.load_config(conf) for conf in PROJECT_DIR.rglob('*.yaml')]
+    return [ProjectConfig.from_file(conf) for conf in PROJECT_DIR.rglob("*.yaml")]
